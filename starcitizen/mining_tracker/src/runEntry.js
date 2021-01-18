6af19c7e1a35a4d569cbbd54ba0a338a9ce25071
AnEmortalKid <janmonterrubio@gmail.com>
2021-01-17 19:46:04 -0600

    cleanup

--- starcitizen/mining_tracker/src/runEntry.js
+++
@@ -1,7 +0,0 @@
-export default class RunEntry {
-  constructor(location, duration, yieldAmount) {
-    this.location = location;
-    this.duration = duration;
-    this.yieldAmount = yieldAmount;
-  }
-}
